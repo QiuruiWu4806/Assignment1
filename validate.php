639543669a1d6586f816b3281689c744b66fd2dd
qiurwu <34172962-qiurwu@users.noreply.replit.com>
2024-05-21 22:28:39 +0000

    Logged_in virable
    Create logged_in veirable to keep track of login stats

--- validate.php
+++ validate.php
@@ -4,9 +4,11 @@ $valid_username = "qiurwu";
 $valid_password = "pass";
 
 $username = $_REQUEST['username'];
+$_SESSION['username'] = $username;
 $password = $_REQUEST['password'];
 
 if ($valid_username == $username && $valid_password == $password) {
+  $_SESSSION['logged_in'] = true;
   header ('location: /index.php');
 } else {
 
