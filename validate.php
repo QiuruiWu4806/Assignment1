ba38408cc335dd6ec8c539e94822cc3862df81f8
qiurwu <34172962-qiurwu@users.noreply.replit.com>
2024-05-21 22:26:18 +0000

    Check username and password
    Get the input from login.php and compare them to set value to validate login info, and keep track of failed login attempt

--- validate.php
+++ validate.php
@@ -3,5 +3,21 @@
 $valid_username = "qiurwu";
 $valid_password = "pass";
 
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+if ($valid_username == $username && $valid_password == $password) {
+  header ('location: /index.php');
+} else {
+
+  if (!isset($_SESSION['failed_attempts'])){
+    $_SESSION['failed_attempts'] = 1;
+  } else {
+    $_SESSION['failed_attempts'] ++;
+  }
+
+  echo "This is unsuccessful attempt number " . $_SESSION['failed_attempts'];
+
+}
 
 ?>
